97b42634ee3b963acf583506c9fc0ae0e9fcdb06
Gieger <4070685@vtsd.com>
2020-01-16 11:17:10 -0500

    1.0

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,10 @@
 /* Put code for the task below */
+
+$(document).ready(function(){
+  $("#imageTask1").mouseover(function(){
+    $("#buttonTask1").css("display", "inline");
+  });
+  $("#buttonTask1").click(function(){
+    $("#imageTask1").css("display", "none");
+  });
+});
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,16 @@
 /* Put code for the task below */
+
+$(document).ready(function(){
+  $("#button-blue").click(function(){
+    $("#task2-table").css("background", "#001D97")
+    $("#task2-table").css("color", "white")
+  });
+  $("#button-gold").click(function(){
+    $("#task2-table").css("background", "gold")
+    $("#task2-table").css("color", "black")
+  });
+  $("#button-silver").click(function(){
+    $("#task2-table").css("background", "silver")
+    $("#task2-table").css("color", "black")
+  });
+});
